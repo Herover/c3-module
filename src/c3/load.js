1597ee7d4d91729fa1a90840dbb75ded5138f39f
Herover <hi+git@leonora.app>
2023-01-04 13:00:47 +0100

    Include data file directly instead of async loading it
    Makes the binary much larger, but simplifies use of the lib.

--- src/c3/load.js
+++ src/c3/load.js
@@ -1,8 +1,10 @@
 import { c3_init } from "./init.js";
 import { c3_api } from "./api.js";
+import * as c3data from "../../data/xkcd/c3_data.json";
 
-export const load = async function(uri) {
-  const data = await (await fetch(uri)).json();
+export const load = function() {
+  console.log(c3data)
+  const data = c3data;
   const c3 = c3_init(data);
   return c3_api(c3);
 }
