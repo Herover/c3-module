c1863642fc89a6f418ab51d65a9f8dd6e5278e04
Herover <hi+git@leonora.app>
2023-01-18 21:43:49 +0100

    Remove unneeded load function, simplify some stuff

--- src/c3/init.ts
+++ src/c3/init.ts
@@ -1,40 +1,33 @@
 import { lab } from "d3-color";
+import * as json from "../../data/xkcd/c3_data.json";
 
-export function c3_init(json) {
-  const data = {};
-  var i, C, W, T, A, ccount, tcount;
+// parse colors
+export const color: {l: number, a: number, b: number, opacity?: number}[] = [];
+for (let i=0; i<json.color.length; i+=3) {
+  color[i/3] = lab(json.color[i], json.color[i+1], json.color[i+2]);
+}
+const C = color.length;
 
-  // parse colors
-  data.color = [];
-  for (i=0; i<json.color.length; i+=3) {
-    data.color[i/3] = lab(json.color[i], json.color[i+1], json.color[i+2]);
-  }
-  C = data.color.length;
+// parse terms
+export const terms: string[] = json.terms;
+const W = terms.length;
 
-  // parse terms
-  data.terms = json.terms;
-  W = data.terms.length;
-
-  // parse count table
-  data.T = T = [];
-  for (var i=0; i<json.T.length; i+=2) {
-    T[json.T[i]] = json.T[i+1];
-  }
-
-  // construct counts
-  data.color.count = ccount = []; for (i=0; i<C; ++i) ccount[i] = 0;
-  data.terms.count = tcount = []; for (i=0; i<W; ++i) tcount[i] = 0;
-  Object.keys(T).forEach(function(idx) {
-    var c = Math.floor(idx / W),
-        w = Math.floor(idx % W),
-        v = T[idx] || 0;
-    ccount[c] += v;
-    tcount[w] += v;
-  });
-
-  // parse word association matrix
-  data.A = A = json.A;
-
-  return data;
+// parse count table
+export const T: number[] = [];
+for (let i=0; i<json.T.length; i+=2) {
+  T[json.T[i]] = json.T[i+1];
 }
 
+// construct counts
+export const colorCount: number[] = []; for (let i=0; i<C; ++i) colorCount[i] = 0;
+export const termsCount: number[] = []; for (let i=0; i<W; ++i) termsCount[i] = 0;
+for (let idx = 0; idx < T.length; idx++) {
+  let c = Math.floor(idx / W),
+      w = Math.floor(idx % W),
+      v = T[idx] || 0;
+  colorCount[c] += v;
+  termsCount[w] += v;
+}
+
+// parse word association matrix
+export const A: number[] = json.A;
